ab723f0cdb64f69044e41103e96cc5a5017c40f9
Hendi Santika <hendisantika@gmail.com>
2022-03-14 08:07:49 +0700

    #10 - Add sendSMS API in SMSController

--- src/main/java/com/hendisantika/controller/SMSController.java
+++ src/main/java/com/hendisantika/controller/SMSController.java
@@ -1,8 +1,15 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.Sms;
 import com.hendisantika.service.SmsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,11 +21,28 @@ import org.springframework.web.bind.annotation.RestController;
  * Date: 14/03/22
  * Time: 08.03
  */
+@Slf4j
 @RestController
+@RequestMapping("api/otp")
 public class SMSController {
     private final String TOPIC_DESTINATION = "/lesson/sms";
     @Autowired
     private SmsService service;
     @Autowired
     private SimpMessagingTemplate webSocket;
+
+    //You can send SMS in verified Number
+    @PostMapping()
+    public ResponseEntity<Boolean> sendSMS(@RequestBody Sms sms) {
+        try {
+            log.info("hello");
+            service.send(sms);
+            log.info("hello");
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        webSocket.convertAndSend(TOPIC_DESTINATION, getTimeStamp() + ": SMS has been sent!: " + sms.getTo());
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
 }

5c081409409e37173fb37bb5abe2b5770094a02f
Hendi Santika <hendisantika@gmail.com>
2022-03-14 08:02:00 +0700

    #7 - Add Sms DTO

---
+++ src/main/java/com/hendisantika/dto/Sms.java
@@ -0,0 +1,17 @@
+package com.hendisantika.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-twilio-otp
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/03/22
+ * Time: 08.01
+ */
+@Data
+public class Sms {
+    private String to;
+}

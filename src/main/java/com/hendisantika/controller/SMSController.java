59ac5fd0f63576f6e9c8abec950530328adaf9b2
Hendi Santika <hendisantika@gmail.com>
2022-03-14 08:10:36 +0700

    #11 - Add smsCallback API in SMSController

--- src/main/java/com/hendisantika/controller/SMSController.java
+++ src/main/java/com/hendisantika/controller/SMSController.java
@@ -5,12 +5,17 @@ import com.hendisantika.service.SmsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -44,5 +49,17 @@ public class SMSController {
         }
         webSocket.convertAndSend(TOPIC_DESTINATION, getTimeStamp() + ": SMS has been sent!: " + sms.getTo());
         return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/sms-callback", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces =
+            MediaType.APPLICATION_JSON_VALUE)
+    public void smsCallback(@RequestBody MultiValueMap<String, String> map) {
+        service.receive(map);
+        webSocket.convertAndSend(TOPIC_DESTINATION, getTimeStamp() + ": Twilio has made a callback request! Here are " +
+                "the contents: " + map.toString());
+    }
+
+    private String getTimeStamp() {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
     }
 }

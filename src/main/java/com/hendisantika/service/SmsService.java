ebd2f6139eacb7005ef16dd4b7379c0e91bec428
Hendi Santika <hendisantika@gmail.com>
2022-03-14 08:00:19 +0700

    #5 - Add SmsService

---
+++ src/main/java/com/hendisantika/service/SmsService.java
@@ -0,0 +1,21 @@
+package com.hendisantika.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-twilio-otp
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/03/22
+ * Time: 07.59
+ */
+@Component
+public class SmsService {
+    private final String ACCOUNT_SID = "AC1272336dfaf920cf19319a0b06280b7f";
+
+    private final String AUTH_TOKEN = "043f16a4bb78aae55f3b1e424d03d651";
+
+    private final String FROM_NUMBER = "+14043417638";
+}

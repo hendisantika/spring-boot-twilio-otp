ab63bff7315ec5159360dd76a1c0edb928f9eb99
Hendi Santika <hendisantika@gmail.com>
2022-03-14 08:11:49 +0700

    #12 - Add TokenController API in TokenController

---
+++ src/main/java/com/hendisantika/controller/TokenController.java
@@ -0,0 +1,32 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.dto.OTP;
+import com.hendisantika.dto.TempOTP;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-twilio-otp
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/03/22
+ * Time: 08.10
+ */
+@RestController
+public class TokenController {
+    @PostMapping("/otp")
+    public ResponseEntity<String> ValidateToken(@RequestBody TempOTP otp) {
+
+        int sentOTP = OTP.getOtp();
+        System.out.println(sentOTP + ":" + otp.getOtp());
+        if (sentOTP == otp.getOtp()) {
+            return new ResponseEntity<>("Success otp", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("failure", HttpStatus.UNAUTHORIZED);
+    }
+}

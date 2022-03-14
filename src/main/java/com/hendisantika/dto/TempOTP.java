e50751d57e359e3bd61c6e7e5c67d7d569e953b7
Hendi Santika <hendisantika@gmail.com>
2022-03-14 07:59:31 +0700

    #4 - Add TempOTP DTO

---
+++ src/main/java/com/hendisantika/dto/TempOTP.java
@@ -0,0 +1,17 @@
+package com.hendisantika.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-twilio-otp
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/03/22
+ * Time: 07.59
+ */
+@Data
+public class TempOTP {
+    int otp;
+}
